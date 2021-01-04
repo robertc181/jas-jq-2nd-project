03bff4217c4a0705938f7f57f9e02f6d5ab0e380
robertc181 <rdancole@gmail.com>
2021-01-04 10:28:07 +0000

    initial commit

---
+++ specs/calcSpec.js
@@ -0,0 +1,7 @@
+descrbe("calculator" ,function(){
+    describe("addition tests" ,function(){
+        it("should return 42" ,function(){
+            expext(addition(20, 22)).toBe(42);
+        });
+    });
+});
